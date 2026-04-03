1589ee8a0aad8610bedd1b16ce979b132326acea
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-04-03 23:53:53 +0800

    chore: add vuetify and use ESM

---
+++ src/web/client/vite.config.ts
@@ -0,0 +1,50 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import Vuetify from 'vite-plugin-vuetify'
+import Fonts from 'unplugin-fonts/vite'
+import path from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        vueDevTools(),
+        Vuetify(),
+        Fonts({
+            fontsource: {
+                families: [
+                    {
+                        name: 'Roboto',
+                        weights: [100, 300, 400, 500, 700, 900],
+                        styles: ['normal', 'italic'],
+                    },
+                ],
+            },
+        })
+    ],
+    root: __dirname,
+    build: {
+        outDir: path.resolve(__dirname, 'dist'),
+        emptyOutDir: true
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('src', import.meta.url))
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
+    },
+    server: {
+        port: 3000
+    }
+})
