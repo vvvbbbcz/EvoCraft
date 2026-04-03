73cb04d79009866593e90c283ca97d8acc8a9cc5
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-04-03 23:57:30 +0800

    feat: base vuetify interface

--- src/web/client/src/main.ts
+++ src/web/client/src/main.ts
@@ -1,11 +1,20 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+
+import 'unfonts.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(createVuetify({
+    theme: {
+        defaultTheme: 'system',
+    },
+}))
 
 app.mount('#app')
--- src/web/client/src/router/index.ts
+++ src/web/client/src/router/index.ts
@@ -1,21 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Bots from '../views/Bots.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
-            name: 'home',
-            component: HomeView,
+            name: 'Home',
+            redirect: '/bots',
         },
         {
-            path: '/about',
-            name: 'about',
+            path: '/bots',
+            name: 'Bots',
+            component: Bots,
+        },
+        {
+            path: '/skills',
+            name: 'Skills',
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue'),
+            component: () => import('../views/Skills.vue'),
         },
     ],
 })
